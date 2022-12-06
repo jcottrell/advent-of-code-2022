171344cff10b499288bb9bfdfc7b047c2f9e6afb
Joshua Cottrell <joshua.e.cottrell@pm.me>
2022-12-06 15:19:46 -0500

    Day 6, part 2
    Had to parameterize the hard-coded marker length from the first part
    then pass it in for the first part and the calls to the second part,
    encouraging!

--- day-06/code.rkt
+++ day-06/code.rkt
@@ -4,23 +4,32 @@
 
 ; Part 1
 ;
-(define (file-name->last-position-of-marker file-name)
+(define (file-name->last-position-of-marker marker-window-size file-name)
   (let* ([in (open-input-file file-name #:mode 'text)])
     (call/cc (lambda (quick-exit)
                (letrec ([consume (lambda (counter so-far)
                                    (cond [(eof-object? in) (close-input-port in)]
-                                         [(and (= 4 (length so-far))
+                                         [(and (= marker-window-size (length so-far))
                                                (not (check-duplicates so-far)))
                                           (quick-exit (begin (close-input-port in)
                                                              counter))]
                                          [else (consume (add1 counter)
                                                         (take-right (append so-far (list (read-string 1 in)))
-                                                                    (min (add1 (length so-far)) 4)))]))])
-                 (consume 1 (list (read-string 1 in)))))))))
+                                                                    (min (add1 (length so-far))
+                                                                         marker-window-size)))]))])
+                 (consume 1 (list (read-string 1 in))))))))
 
-;(file-name->last-position-of-marker "example1.txt") ; 7
-;(file-name->last-position-of-marker "example2.txt") ; 5
-;(file-name->last-position-of-marker "example3.txt") ; 6
-;(file-name->last-position-of-marker "example4.txt") ; 10
-;(file-name->last-position-of-marker "example5.txt") ; 11
-;(file-name->last-position-of-marker "input.txt")    ; 1356
+;(file-name->last-position-of-marker 4 "example1.txt") ; 7
+;(file-name->last-position-of-marker 4 "example2.txt") ; 5
+;(file-name->last-position-of-marker 4 "example3.txt") ; 6
+;(file-name->last-position-of-marker 4 "example4.txt") ; 10
+;(file-name->last-position-of-marker 4 "example5.txt") ; 11
+;(file-name->last-position-of-marker 4 "input.txt")    ; 1356
+
+; Part 2
+;(file-name->last-position-of-marker 14 "example1.txt") ; 19
+;(file-name->last-position-of-marker 14 "example2.txt") ; 23
+;(file-name->last-position-of-marker 14 "example3.txt") ; 23
+;(file-name->last-position-of-marker 14 "example4.txt") ; 29
+;(file-name->last-position-of-marker 14 "example5.txt") ; 26
+;(file-name->last-position-of-marker 14 "input.txt")    ; 2564
